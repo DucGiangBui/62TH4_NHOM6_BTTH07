98f11273cc690a34743e7ccf1610f40d60e16ad6
PhamQuyen27 <2051063534@e.tlu.edu.vn>
2022-03-26 10:39:45 +0700

    Yc2

--- Yeu Cau 02/js/main.js
+++ Yeu Cau 02/js/main.js
@@ -8,37 +8,78 @@ $(document).ready(function(){
             }
             return false;
         }
+
         function checkPW() {
             let pw = $("#txtPW").val();
             let pwRegex = /^[a-zA-z0-9]{7,12}$/;
-            if (pwRegex.test(pw.value)) {
+            if (pwRegex.test(pw)) {
                 return true;
             }
             return false;
         }
-        function checkName() {
-            let Name = $("txtName").val();
-            let NameRegex =/^[a-zA-ZÀÁÂÃÈÉÊÌÍÒÓÔÕÙÚĂĐĨŨƠàáâãèéêìíòóôõùúăđĩũơƯĂẠẢẤẦẨẪẬẮẰẲẴẶẸẺẼỀỀỂẾưăạảấầẩẫậắằẳẵặẹẻẽềềểếỄỆỈỊỌỎỐỒỔỖỘỚỜỞỠỢỤỦỨỪễệỉịọỏốồổỗộớờởỡợụủứừỬỮỰỲỴÝỶỸửữựỳỵỷỹ\s\W|_]+$/;
-            if (NameRegex.test(Name.value)) {
-                 return true;
+
+        function checkName(){
+            let name = $("#txtName").val();
+            let nameRegex = /^[a-z0-9A-ZÀÁÂÃÈÉÊÌÍÒÓÔÕÙÚĂĐĨŨƠàáâãèéêìíòóôõùúăđĩũơƯĂẠẢẤẦẨẪẬẮẰẲẴẶẸẺẼỀỀỂẾưăạảấầẩẫậắằẳẵặẹẻẽềềểếỄỆỈỊỌỎỐỒỔỖỘỚỜỞỠỢỤỦỨỪễệỉịọỏốồổỗộớờởỡợụủứừỬỮỰỲỴÝỶỸửữựỳỵỷỹ\s\W|_]+$/;
+            if (nameRegex.test(name)){
+                return true;
             }
             return false;
         }
+
         function checkAddress(){
-            let Address = $("txtAddress").val();
+            let Address = $("#txtAdd").val();
             let AddressRegex = /^[a-z0-9A-ZÀÁÂÃÈÉÊÌÍÒÓÔÕÙÚĂĐĨŨƠàáâãèéêìíòóôõùúăđĩũơƯĂẠẢẤẦẨẪẬẮẰẲẴẶẸẺẼỀỀỂẾưăạảấầẩẫậắằẳẵặẹẻẽềềểếỄỆỈỊỌỎỐỒỔỖỘỚỜỞỠỢỤỦỨỪễệỉịọỏốồổỗộớờởỡợụủứừỬỮỰỲỴÝỶỸửữựỳỵỷỹ\s\W|_]+$/;
-            if(AddressRegex.test(Address.value)){
+            if(AddressRegex.test(Address)){
                 return true
             }
             return false
         }
-        function checkCountry() {
-            let country = d$("country").val();
-            if (country.value != "0") {
+        // function checkCountry() {
+        //     let country = d$("#country").val();
+        //     if (country!= "0") {
+        //         return true;
+        //     } 
+        //     return false;
+        // }
+
+        function checkZipCode(){
+            let zipCode = $("#txtZip").val();
+            let zipCodeRegex = /^[0-9]{5,7}$/;
+            if (zipCodeRegex.test(zipCode)){
                 return true;
-            } 
+            }
             return false;
         }
+
+        function checkEmail(){
+            let email = $("#txtEmail").val();
+            let emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,6}$/;
+            if (emailRegex.test(email)){
+                return true;
+            }
+            return false;
+        }
+
+        function checkEmail(){
+            let email = $("#txtEmail").val();
+            let emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,6}$/;
+            if (emailRegex.test(email)){
+                return true;
+            }
+            return false;
+        }
+
+        function checkEmail(){
+            let email = $("#txtEmail").val();
+            let emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,6}$/;
+            if (emailRegex.test(email)){
+                return true;
+            }
+            return false;
+        }
+    
+    
         $("#btnRegister").click(function(e){
             e.preventDefault();
             if(checkUserID()){
@@ -73,13 +114,31 @@ $(document).ready(function(){
                 $("#statusOfAdd").text('Address không hợp lệ !')
                 $("#statusOfAdd").css('color','red')
             }
-            if(checkCountry()){
-                $("#statusOfCountry").text('Country hợp lệ !')
-                $("#statusOfCountry").css('color','blue')
+            // if(checkCountry()){
+            //     $("#statusOfCountry").text('Country hợp lệ !')
+            //     $("#statusOfCountry").css('color','blue')
+            // }
+            // else{
+            //     $("#statusOfCountry").text('Country không hợp lệ !')
+            //     $("#statusOfCountry").css('color','red')
+            // }
+
+            if(checkZipCode()){
+                $("#statusOfZip").text('Zip Code hợp lệ !')
+                $("#statusOfZip").css('color','blue')
             }
             else{
-                $("#statusOfCountry").text('Country không hợp lệ !')
-                $("#statusOfCountry").css('color','red')
+                $("#statusOfZip").text('Zip Code không hợp lệ !')
+                $("#statusOfZip").css('color','red')
+            }
+
+            if(checkEmail()){
+                $("#statusOfEmail").text('Email hợp lệ !')
+                $("#statusOfEmail").css('color','blue')
+            }
+            else{
+                $("#statusOfEmail").text('Email không hợp lệ !')
+                $("#statusOfEmail").css('color','red')
             }
         })
     })
